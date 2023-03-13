189f0e28b0eecf58f750d785de782ddc2d6eea7b
Racs Tamás <cant0r.universal@gmail.com>
2023-03-13 11:03:25 +0100

    Setup basic NUKE system

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Linq;
+using Serilog;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
-
-class Build : NukeBuild
+partial class Build : NukeBuild
 {
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
@@ -19,26 +21,48 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.CompileAll);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => _ => _
-        .Before(Restore)
+    [Solution]
+    readonly Solution Solution;
+
+    Target CompileBackend => _ => _
+        .Executes(() =>
+        {
+            Log.Information("Building .NET Backend..");
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)); 
+            Log.Information("Built .NET Backend.");
+        });
+
+    Target CompileWebApi => _ => _
+        .Executes(() =>
+        {
+            Log.Warning("Here you'll be calling your build scripts for the WebAPI.");
+        });
+
+    Target CompileFrontend => _ => _
+        .Executes(() =>
+        {
+            Log.Warning("Here you'll be calling your build scripts for the Frontend.");            
+        });
+
+    Target CompileAll => _ => _
+        .DependsOn(CompileBackend, CompileWebApi, CompileFrontend)
         .Executes(() =>
         {
         });
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
-
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
+    Target ValidateCLI => _ => _
+        .DependsOn(CompileBackend)
+        .Executes(() => {
+            DotNetRun(_ => _
+            .EnableNoBuild()
+            .SetProjectFile(YouToddlerCliCsprojPath));
         });
 
 }
