b201db43900aca7ee5f864f47d8369df8987c113
Racs Tamás <cant0r.universal@gmail.com>
2023-04-11 12:13:19 +0200

    IMplemented YouToddlerRunner

--- src/Libs/YouToddler/Runner/YouToddlerRunner.cs
+++ src/Libs/YouToddler/Runner/YouToddlerRunner.cs
@@ -1,21 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Serilog;
 using System.Diagnostics;
-using System.IO.Abstractions;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace YouToddler.Runner
 {
-    internal class YouToddlerRunner
+    public static class YouToddlerRunner
     {
-        private ProcessStartInfo _toolProcess;
-        private IFileSystem _fileSystem;
+        private static ProcessStartInfo _toolProcess;
+        private static string _ytDlpBinaryName = OperatingSystem.IsWindows() ? "yt-dlp.exe" : "yt-dlp";
+        private static string _ffmpegBinaryName = OperatingSystem.IsWindows() ? "ffmpeg.exe" : "ffmpeg";
 
-        public YouToddlerRunner(IFileSystem fileSystem)
+        static YouToddlerRunner()
         {
-            _fileSystem = fileSystem;
+            _toolProcess = new ProcessStartInfo();
+            _toolProcess.UseShellExecute = false;
+            _toolProcess.RedirectStandardOutput = true;
+            _toolProcess.RedirectStandardError = true;
+            _toolProcess.CreateNoWindow = true;
+
+            bool dependenciesInstalled = true;
+            dependenciesInstalled &= IsDependencyInstalledOnPath(_ytDlpBinaryName, "--version");
+            dependenciesInstalled &= IsDependencyInstalledOnPath(_ffmpegBinaryName, "-h");
+
+            if (!dependenciesInstalled) 
+            {
+                throw new InvalidOperationException($"YT-DLP and/or its dependencies are not installed. They cannot be found on PATH.");
+            }
+
+            _toolProcess.FileName = _ytDlpBinaryName;
+        }
+
+        public static (int exitCode, string output) ExecuteYtDlp(string arguments)
+        {
+            string output = "";
+            _toolProcess.Arguments = arguments;
+            using (Process p = Process.Start(_toolProcess)!)
+            {
+                p.WaitForExit(TimeSpan.FromHours(1));
+                if (p.ExitCode != 0) 
+                {
+                    using (StreamReader sr = p.StandardError)
+                    {
+                        output = sr.ReadToEnd();
+                        using (StreamReader sro = p.StandardOutput)
+                        {
+                            output += sro.ReadToEnd();
+                            return (p.ExitCode, output);
+                        }
+                    }
+                }
+                else
+                {
+                    using (StreamReader sr = p.StandardOutput)
+                    {
+                        output = sr.ReadToEnd();
+                        return (p.ExitCode, output);
+                    }
+                }
+            }
+        }
+
+        private static bool IsDependencyInstalledOnPath(string binaryName, string arguments)
+        {
+            string output = "";
+            _toolProcess.FileName = binaryName;
+            _toolProcess.Arguments = arguments;
+            using (Process? p = Process.Start(_toolProcess))
+            {
+                if (p is null)
+                {
+                    Log.Fatal($"{binaryName} (and/or its dependencies) is not installed on PATH! Please install {binaryName} and its dependencies on the host system.");
+                    return false;
+                }
+                using (StreamReader sr = p.StandardOutput)
+                {
+                    output = sr.ReadToEnd();
+
+                    return !string.IsNullOrEmpty(output);
+                }
+            }
         }
     }
 }
---
+++ tests/YouToddlerLib.Tests/Integration/YouToddlerRunnerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YouToddler.Runner;
+
+namespace YouToddlerLib.Tests.Integration
+{
+    [TestFixture]
+    public class YouToddlerRunnerTests
+    {
+        [Test]
+        public void ExecuteYtDlp_GetYtDlpVersion_ReturnsVersionExitCodeZero()
+        {
+            (int exitCode, string output) result = YouToddlerRunner.ExecuteYtDlp("--version");
+            Assert.That(result.exitCode, Is.EqualTo(0));
+            Assert.That(result.output, Is.Not.Empty);
+        }
+    }
+}
