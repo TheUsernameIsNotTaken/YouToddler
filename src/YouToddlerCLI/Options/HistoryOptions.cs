d2e2a82882ff625f4d231708b75c5913dcce5c26
Racs Tamás <cant0r.universal@gmail.com>
2023-04-13 12:55:52 +0200

    Created option classes for cmdoptions

---
+++ src/YouToddlerCLI/Options/HistoryOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouToddlerCLI.Options
+{
+    [Verb("history", false, new[] { "h" }, HelpText = "Returns log history.")]
+    public class HistoryOptions
+    {
+        [Option('d', "delete", HelpText = "Erase logs", Default = false, Required = false)]
+        public bool DeleteHistory { get; set; }
+    }
+}
