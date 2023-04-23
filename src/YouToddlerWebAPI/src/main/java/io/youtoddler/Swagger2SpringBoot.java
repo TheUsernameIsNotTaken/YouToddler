a314da79f603d81f4697d1c987aff39807096ac6
TheUsernameIsNotTaken <vagnermate@gmail.com>
2023-04-23 14:29:42 +0200

    Refactored packagename part 3

--- src/YouToddlerWebAPI/src/main/java/io/youtoddler/Swagger2SpringBoot.java
+++ src/YouToddlerWebAPI/src/main/java/io/youtoddler/Swagger2SpringBoot.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 @EnableOpenApi
-@ComponentScan(basePackages = { "io.swagger", "io.swagger.api" , "io.swagger.configuration"})
+@ComponentScan(basePackages = { "io.youtoddler", "io.youtoddler.api" , "io.youtoddler.configuration"})
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
