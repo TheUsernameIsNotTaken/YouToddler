a2fbaf1235836033214c2356fc0e6dc3fbbfdec7
Racs Tamás <cant0r.universal@gmail.com>
2023-04-26 20:18:54 +0200

    C

--- build/Build.cs
+++ build/Build.cs
@@ -66,7 +66,7 @@ partial class Build : NukeBuild
         .DependsOn(BuildCli)
         .Executes(() =>
         {
-            PowerShell(@$"Write-Output ""{DOCKER_PASSWORD}"" > docker login -u {DOCKER_USERNAME} --password-stdin", YouToddlerWebApiPath, logOutput: false, logInvocation: false);
+            PowerShell(@$"Write-Host ""{DOCKER_PASSWORD}"" > docker login -u {DOCKER_USERNAME} --password-stdin", YouToddlerWebApiPath, logOutput: false, logInvocation: false);
 
             DockerBuild(_ => _
                 .SetPath(RootDirectory / "src/")
